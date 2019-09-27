8d524ee4f852f05e328d88b0c1fddaea3d6cc252
Matthew Colwell <matrocksteady@gmail.com>
2019-09-26 23:01:58 -0400

    whitespace cleanup

--- madlibs.py
+++ madlibs.py
@@ -8,19 +8,19 @@ wordListRx = re.findall(r'(\w+)(\s|[?.!](\s?))', content)
 builderList = []
 
 for i in range(len(wordListRx)):
-        rules = ['ADJECTIVE' in wordListRx[i],
-                 'NOUN' in wordListRx[i],
-                 'VERB' in wordListRx[i],
-                 'ADVERB' in wordListRx[i]]
+    rules = ['ADJECTIVE' in wordListRx[i],
+             'NOUN' in wordListRx[i],
+             'VERB' in wordListRx[i],
+             'ADVERB' in wordListRx[i]]
     
-        if any(rules):
-            partOfSpeechTupleToList = [''.join(i) for i in wordListRx[i]]
-            userResponse = input(f"Enter an {partOfSpeechTupleToList[0].lower()}: ")
-            tupleToList = [''.join(i) for i in wordListRx[i]]
-            builderList += userResponse
-            builderList += tupleToList[1]
-        else:
-            builderList += [''.join(i) for i in wordListRx[i]]
+    if any(rules):
+        partOfSpeechTupleToList = [''.join(i) for i in wordListRx[i]]
+        userResponse = input(f"Enter an {partOfSpeechTupleToList[0].lower()}: ")
+        tupleToList = [''.join(i) for i in wordListRx[i]]
+        builderList += userResponse
+        builderList += tupleToList[1]
+    else:
+        builderList += [''.join(i) for i in wordListRx[i]]
 
 finalString = ''.join(builderList)
 print(finalString)
